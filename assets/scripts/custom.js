7ce8683c20b27d4a40f64032b9372dd9afd8fd4d
Christopher Amirian <christopher.amirian@gmail.com>
2014-03-15 18:06:44 +0400

    Second change test

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1194,6 +1194,7 @@ function showJarsLevel3() {
 }
 
 
+
 // For the date now
 Date.prototype.today = function () { 
     return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
